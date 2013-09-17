8d6ad08f0b4b7c4facfdff54be1e92fa2e3f6311
Craig Edmunds <craig.edmunds@gmail.com>
2013-09-17 08:01:16 +0100

    Example umongo brew script

---
+++ umongo.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class UMongo < Formula
+  homepage 'http://www.edgytech.com/umongo'
+  url 'https://github.com/agirbal/umongo'
+  version 'tip'
+
+  def install
+    # Instead of copying things piecemeal, I just reclone arcanist in.
+    # This lets `arc upgrade` work (which is also a violation of Homebrew
+    # policies).
+    system 'git', 'clone', url
+    
+    # cd 'arcanist/externals/includes' do
+    #   system 'git', 'clone', 'git://github.com/facebook/libphutil.git'
+    # end
+    # prefix.install 'arcanist'
+    # completion_d = prefix+'etc/bash_completion.d'
+    # mkdir_p completion_d
+    # ln_s (prefix+'arcanist/resources/shell/bash-completion'), (completion_d+'arcanist')
+    # (bin+'arc').write <<-EOS.undent
+    # #!/bin/sh
+    # "#{prefix}/arcanist/bin/arc" "$@"
+    # EOS
+  end
+end
